e17c444a6b6efd94dab9d2289054350b20a20a6a
mzy0624 <mzy0624@gmail.com>
2024-07-06 01:50:19 +0800

    multiple songs

--- corner/js/music.js
+++ corner/js/music.js
@@ -1,47 +1,113 @@
+let song_name    = ['七里香 - 周杰伦', '海阔天空 - BEYOND'];
+let song_count   = song_name.length;
+let cur_song     = 0;
 let started      = false;
-let audio        = new BaseElement('audio', '', {'src' : 'files/music.mp3'});
-let cover        = new Img('files/cover.png',   {'id' : 'cover', 'class' : 'hidden', 'style' : 'width: 64px'});
-let music_name   = new Small('点击播放音乐');
-let controller   = new Button('▶️',              {'style' : 'background: transparent; border: none'});
-let progress     = new BaseElement('input', '', {'type' : 'range', 'value' : '0', 'disabled' : 'true'});
-let cur_time     = new Span('0:00',             {'style' : ['font-size: 14px', 'margin-right: 2px']});
-let total_time   = new Span('0:00',             {'style' : ['font-size: 14px', 'margin-left: 2px']});
-let cur_lyric    = new Para('',                 {'style' : ['margin: 0 10px', 'font-size: 10px']});
-let next_lyric   = new Para('',                 {'style' : ['margin: 0 10px', 'font-size: 10px', 'color: #9c9c9c;']});
+let audio        = new BaseElement('audio', '',  {'src' : 'files/musics/0.mp3'});
+let cover        = new Img('files/musics/0.png', {'id' : 'cover', 'class' : 'hidden', 'style' : 'height: 95px'});
+let music_name   = new Small('点击播放音乐',       {'style' : 'margin-bottom: -1em'});
+let prev_song    = new Button('⏪',              {'style' : 'background: transparent; border: none'})
+let play_pause   = new Button('▶️',               {'style' : 'background: transparent; border: none'});
+let next_song    = new Button('⏩',              {'style' : 'background: transparent; border: none'});
+let progress     = new BaseElement('input', '',  {'type'  : 'range', 'value' : '0', 'disabled' : 'true'});
+let cur_time     = new Span('0:00',              {'style' : ['font-size: 14px', 'margin-right: 2px']});
+let total_time   = new Span('0:00',              {'style' : ['font-size: 14px', 'margin-left: 2px']});
+let cur_lyric    = new Para('',                  {'style' : ['margin: 0 10px', 'font-size: 10px']});
+let next_lyric   = new Para('',                  {'style' : ['margin: 0 10px', 'font-size: 10px', 'color: #9c9c9c;']});
+let is_seeking   = false;
+let lyrics       = [];
 let music_player = new Table([[
-    cover, 
-    new Div([
-        audio, new Div([music_name, controller]),
+    cover, new Div([
+        audio, music_name,
+        new Div([prev_song, play_pause, next_song]),
         new Div([cur_time, progress, total_time], {'class' : 'music_controller'}),
         new Div([cur_lyric, next_lyric], {'class' : 'lyrics'})
     ], {'class' : 'music_player'})
-]], {'style' : 'min-height: 76px'});
+]], {'style' : 'min-height: 100px'});
 append_elem('masthead', music_player);
 
-let is_seeking = false;
-let lyrics = [];
+function change_song() {
+    audio.elem.src = `files/musics/${cur_song}.mp3`;
+    cover.elem.src = `files/musics/${cur_song}.png`;
+    music_name.cover_innerhtml(song_name[cur_song]);
+    play_pause.cover_innerhtml('⏸️');
+    cur_time.cover_innerhtml('0:00');
+    total_time.cover_innerhtml(format_time(audio.elem.duration));
+    progress.elem.value = 0;
+    cur_lyric.cover_innerhtml('');
+    next_lyric.cover_innerhtml('');
+    fetch(`files/musics/${cur_song}.lrc`).then(
+        response => response.text()
+    ).then(
+        data => parse_lrc(data)
+    );
+    audio.elem.addEventListener('loadedmetadata', () => {
+        total_time.cover_innerhtml(format_time(audio.elem.duration));
+    });
+    audio.elem.play();
+}
 
-// Fetch lyrics and parse them
-fetch('files/lyrics.lrc').then(response => response.text()).then(data => lyrics = parse_lrc(data));
+function initialize() {
+    cover.elem.classList.remove('hidden');
+    music_name.cover_innerhtml(song_name[cur_song]);
+    total_time.cover_innerhtml(format_time(audio.elem.duration));
+    progress.elem.disabled = false;  // Enable progress bar
+    fetch('files/musics/0.lrc').then(response => response.text()).then(data => parse_lrc(data));
+    started = true;
+}
 
 // Add event listener for play/pause button
-controller.elem.addEventListener('click', () => {
+play_pause.elem.addEventListener('click', () => {
     if (!started) {
-        let cover_img = document.getElementById('cover');
-        cover_img.classList.remove('hidden');
-        music_name.cover_innerhtml('七里香 - 周杰伦');
-        total_time.cover_innerhtml(format_time(audio.elem.duration));
-        started = true;
-        progress.elem.disabled = false;  // Enable progress bar
+        initialize();
     }
     if (audio.elem.paused) {
         audio.elem.play();
-        controller.cover_innerhtml('⏸️');
+        play_pause.cover_innerhtml('⏸️');
     } 
     else {
         audio.elem.pause();
-        controller.cover_innerhtml('▶️');
+        play_pause.cover_innerhtml('▶️');
     }
+});
+
+// Add event listener for prev song button
+prev_song.elem.addEventListener('click', () => {
+    if (started) {
+        cur_song = (cur_song + song_count - 1) % song_count;
+        change_song();
+    }
+});
+
+// Add event listener for next song button
+next_song.elem.addEventListener('click', () => {
+    if (started) {
+        cur_song = (cur_song + 1) % song_count;
+        change_song();
+    }
+});
+
+function change_song() {
+    audio.elem.src = `files/musics/${cur_song}.mp3`;
+    cover.elem.src = `files/musics/${cur_song}.png`;
+    audio.elem.play();
+    music_name.cover_innerhtml(song_name[cur_song]);
+    play_pause.cover_innerhtml('⏸️');
+    cur_time.cover_innerhtml('0:00');
+    total_time.cover_innerhtml(format_time(audio.elem.duration));
+    progress.elem.value = 0;
+    cur_lyric.cover_innerhtml('');
+    next_lyric.cover_innerhtml('');
+
+    fetch(`files/musics/${cur_song}.lrc`).then(response => response.text()).then(data => parse_lrc(data));
+
+    audio.elem.addEventListener('loadedmetadata', () => {
+        total_time.cover_innerhtml(format_time(audio.elem.duration));
+    });
+}
+
+// Add event listener for when the audio ends
+audio.elem.addEventListener('ended', () => {
+    play_pause.cover_innerhtml('▶️');
 });
 
 audio.elem.addEventListener('timeupdate', () => {
@@ -52,9 +118,7 @@ audio.elem.addEventListener('timeupdate', () => {
         progress.elem.value = (currentTime / duration) * 100;
         cur_time.cover_innerhtml(format_time(currentTime));
 
-        const { currentLyric, nextLyric } = get_lyrics(currentTime);
-        cur_lyric.cover_innerhtml(currentLyric);
-        next_lyric.cover_innerhtml(nextLyric);
+        update_lyrics(currentTime);
     }
 });
 
@@ -85,7 +149,7 @@ function format_time(time) {
 // Parse LRC lyrics
 function parse_lrc(lrc) {
     let lines = lrc.split('\n');
-    let lyrics = [];
+    lyrics = [];
 
     for (let line of lines) {
         let match = line.match(/\[(\d+):(\d+).(\d+)\](.*)/);
@@ -98,24 +162,18 @@ function parse_lrc(lrc) {
             lyrics.push({ time, text });
         }
     }
-
-    return lyrics.sort((a, b) => a.time - b.time);
+    
+    lyrics.sort((a, b) => a.time - b.time);
 }
 
-// Get current and next lyrics based on current time
-function get_lyrics(time) {
-    let currentLyric = '';
-    let nextLyric = '';
-
-    for (let i = 0; i < lyrics.length; i++) {
-        if (time >= lyrics[i].time) {
-            currentLyric = lyrics[i].text;
-            nextLyric = lyrics[i + 1] ? lyrics[i + 1].text : '';
-        } 
-        else {
+// Update current and next lyrics based on current time
+function update_lyrics(time) {
+    let i = 0;
+    for (; i < lyrics.length; i++) {
+        if (time < lyrics[i].time) {
             break;
         }
     }
-
-    return { currentLyric, nextLyric };
+    cur_lyric.cover_innerhtml(lyrics[i - 1].text);
+    next_lyric.cover_innerhtml(lyrics[i] ? lyrics[i].text : '');
 }
---
+++ js/span.js
@@ -0,0 +1,76 @@
+class Span extends Div {
+    /**
+     * A button with a spanable list
+     */
+    constructor(contents='', lists=[], attributes={}) {
+        super(contents, attributes);
+        this.lists = lists;
+        if (contents != '') {
+            this.setSpan();
+        }
+    }
+    
+    setSpan() {
+        let lists = this.lists;
+        lists = new Ul(lists, {'class' : 'hidden'});
+        let small = new Small(lists);
+        if (this.lists.length > 0) {
+            let button = this.getButton();
+            button.addEventListener('click', function () {
+                lists.elem.classList.toggle('visible');
+                button.classList.toggle('rotated');
+            });
+            this.append(button);
+        }
+        this.append(small);
+    }
+
+    getButton() {
+        let button = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        button.id = 'toggleButton';
+        setAttributesForElement(button, {
+            'style' : [
+                'margin-left: 5px',
+                'height: 25px',
+                'vertical-align: middle',
+                'transition: all 0.3s'
+            ],
+            'viewBox' : '0 0 10 10',
+        });
+        this.setShapeForButton(button);
+        return button;
+    }
+    
+    setShapeForButton(button) {
+        let triangle = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
+        setAttributesForElement(triangle, {
+            'points' : '3.27,2 3.27,8 8.46,5',
+            'fill' :   '#52adc8',
+        });
+        button.append(triangle);
+    }
+}
+
+class SpanDiv extends Span {
+    constructor(contents, lists) {
+        super(lists);
+        this.prepend(contents);
+        this.setSpan();
+    }
+}
+
+class SH extends Span {
+    constructor(name, since='', list=[], nameCN='') {
+        super('', list);
+        this.prepend(`${icons(name)} ${nameCN ? nameCN : name} ${since != '' ? `<small>(since ${since})</small>` : ''}`);
+        this.setSpan();
+    }
+};
+
+class TA extends Span {
+    constructor(name, list=[]) {
+        super('', list);
+        this.prepend(`${icons(name)} ${getAnchor(name)}`);
+        this.setSpan();
+    }
+}
